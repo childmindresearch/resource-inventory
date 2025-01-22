05b587ec5fc6dcf36cc7945e9070eb7094fa363e
birajstha <birajstha@gmail.com>
2025-01-22 10:42:27 -0600

    rename

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/resource_inventory/', // Replace 'resource_inventory' with your repository name
+  base: '/resource-inventory/', // Replace 'resource_inventory' with your repository name
 });
